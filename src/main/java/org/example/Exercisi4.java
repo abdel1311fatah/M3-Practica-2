590d16d6c6d01f7858c551f82a4aff66895a05e7
abdel1311fatah <abdel1311fatah@gmail.com>
2023-10-28 17:08:58 +0200

    Fer el 2, el 3 i el 4

---
+++ src/main/java/org/example/Exercisi4.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class Exercisi4 {
+    public Exercisi4() {
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,51 @@
+//Abel Ibarra
+
 package org.example;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("1: Exercisi 1");
+        System.out.println("2: Exercisi 2");
+        System.out.println("3: Exercisi 3");
+        System.out.println("4: Exercisi 4");
+        int option = obtindreInt("Que vols fer? ");
+
+        switch (option){
+            case 1:
+                Exercisi1 exercisi1 = new Exercisi1();
+                exercisi1.exercisi1();
+                break;
+            case 2:
+                Exercisi2 exercisi2 = new Exercisi2();
+                exercisi2.exercisi2();
+                break;
+            default:
+                System.out.println("No has introduit un numero valid");
+        }
+
 
     }
+    public static int obtindreInt(String text) {
+        boolean TipusCorrecte;
+        Scanner sc = new Scanner(System.in);
+        int num_usuari = 0;
+
+        do {
+            System.out.print(text);
+            TipusCorrecte = sc.hasNextInt();
+            if (!TipusCorrecte) {
+                sc.nextLine();
+                System.out.println("Error: Valor no válido");
+            } else {
+                num_usuari = sc.nextInt();
+            }
+        } while (!TipusCorrecte);
+
+        return num_usuari;
+    }
+
 }
